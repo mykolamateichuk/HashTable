7db252af1cff453dcf01afff5e458f348c46cc62
mykolamateichuk <103416436+mykolamateichuk@users.noreply.github.com>
2022-04-11 12:08:30 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/lab2_asd2_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2_asd2_2.dir/HashTable.cpp.o"
+  "CMakeFiles/lab2_asd2_2.dir/LinkedList.cpp.o"
+  "CMakeFiles/lab2_asd2_2.dir/main.cpp.o"
+  "lab2_asd2_2"
+  "lab2_asd2_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab2_asd2_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
